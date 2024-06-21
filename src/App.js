90ff72f2c5f5a05940cc81788e9ac79d16c38c62
Suman Sahoo <ssamsomu@gmail.com>
2024-06-21 20:17:08 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import React from 'react';
+import React, { Fragment } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [responseId, setResponseId] = React.useState("");
-  const [responseState, setResponseState] = React.useState([]);
-  
+  const [token, setToken] = React.useState("");
+  const [razorpaykey, setRazorpayKey] = React.useState("");
+  const [paymentId, setPaymentId] = React.useState("");
+  const [signature, setSignature] = React.useState("");
+  const [_id, setId] = React.useState("");
+  const [verification, setVerification] = React.useState(false);
+
 
   const loadScript = (src) => {
     return new Promise((resolve) => {
@@ -21,48 +25,84 @@ function App() {
     });
   }
 
-  const createRazorpayOrder =  (amount) => {
-    const data = JSON.stringify({
-
-      amount: amount*100,
-      currency: 'INR'
-    });
-    
-    let config = {
-      method: 'post',
-      maxBodyLength: Infinity,
-      url: 'http://localhost:5000/orders',
-      headers: { 
-        'Content-Type': 'application/json'
-      },
-      data : data
-    };
-
-    axios.request(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-      handleRazorpayScreen(response.data.amount);
-    }).catch((error) =>{
-      console.error("error at ", error);
+  const getRazorpayKey = (token) => {
+    axios.post('https://api.testbuddy.live/v1/payment/key', {}, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
     })
+      .then(response => {
+        setRazorpayKey(response.data.key);
+      })
+      .catch(error => {
+        console.error("Error at getting Razorpay key:", error);
+      });
   }
 
-  const handleRazorpayScreen = async (amount) => {
+  const verifyOTP = () => {
+    const data = JSON.stringify({
+      mobile: "+912144419015",
+      otp: "8899"
+    });
+
+    axios.post('https://api.testbuddy.live/v1/auth/verifyotp', data, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        setToken(response.data.token);
+      })
+      .catch(error => {
+        console.error("error at ", error);
+      });
+  }
+
+  const createRazorpayOrder = (amount) => {
+    const data = JSON.stringify({
+      packageId: "6613d6fbbf1afca9aa1b519e",
+      pricingId: "662caa2d50bf43b5cef75232",
+      finalAmount: "441",
+      couponCode: "NEET25"
+    });
+
+    axios.post('https://api.testbuddy.live/v1/order/create', data, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+      .then(response => {
+        console.log("Order created:", JSON.stringify(response.data));
+        setId(response.data._id);  
+        handleRazorpayScreen(response.data.amount, response.data.transactionOrderId);
+      })
+      .catch(error => {
+        console.error("Error at order creation:", error);
+      });
+  };
+
+  const handleRazorpayScreen = async (amount, order_id) => {
     const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js');
-    if(!res){
+    if (!res) {
       alert('Razorpay SDK failed to load. Are you online?');
       return;
     }
 
     const options = {
-      key: 'rzp_test_aIYCBi1ZWuZCuU',
+      key: razorpaykey,
       amount: amount,
       currency: 'INR',
       name: 'Suman Sahoo',
       description: 'Payment to Suman Sahoo',
       image: 'https://example.com/your_logo',
+      order_id: order_id,
       handler: function (response) {
-        setResponseId(response.razorpay_payment_id);
+        console.log("signature ", response.razorpay_signature)
+        console.log("payment id ", response.razorpay_payment_id)
+        console.log("order id ", response.razorpay_order_id)
+        setPaymentId(response.razorpay_payment_id);
+        setSignature(response.razorpay_signature);
       },
       prefill: {
         name: 'Suman Sahoo',
@@ -77,38 +117,44 @@ function App() {
     paymentObject.open();
   }
 
-  const paymentFetch = (e) => {
-    e.preventDefault();
+  const verifyPayment = () => {
+    const data = JSON.stringify({
+      transactionId: _id,
+      razorpayPaymentId: paymentId,
+      razorpaySignature: signature
+    });
 
-    const paymentId = e.target.paymentId.value;
-
-    axios.get(`http://localhost:5000/payment/${paymentId}`) 
-    .then((response) => {
-      console.log(response.data);
-      setResponseState(response.data);
-    }).catch((error) => {
-      console.error("error at ", error);
+    axios.post('https://api.testbuddy.live/v1/order/verify', data, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
     })
-  }
-
+      .then(response => {
+        console.log(JSON.stringify(response.data));
+        console.log("Payment verified:", response.data.success)
+        setVerification(response.data.success);
+      })
+      .catch(error => {
+        console.error("Error at order creation:", error);
+      });
+  };
 
   return (
-    <div className="App">
-      <button onClick={() => createRazorpayOrder(100)}>Pay 100Rs</button>
-      {responseId && <p>{responseId}</p>}
-      <h1>This is payment verification form</h1>
-      <form onSubmit={paymentFetch}>
-        <input type="text" name="paymentId" placeholder="Enter payment id" />
-        <button type="submit">Fetch payment</button>
-        {responseState.length!==0 && 
-        <ul>
-          <li>Payment Status: {responseState.status}</li>
-          <li>Payment Method: {responseState.method}</li>
-          <li>Payment Amount: {responseState.amount /100} Rs.</li>
-          <li>Payment Currency: {responseState.currency}</li>
-        </ul>}
-      </form>
-    </div>
+    <Fragment>
+      <main>
+        {token && <p>{token}</p>}
+        <button onClick={verifyOTP}> verify OTP</button>
+        <br />
+        {razorpaykey && <p>{razorpaykey}</p>}
+        <button onClick={getRazorpayKey} >get razorpay key</button>
+        <br />
+        <button onClick={() => createRazorpayOrder(400000)}>Pay</button>
+        <br />
+        {verification && <p>{verification.toString()}</p>}
+        <button onClick={verifyPayment}>verify</button>
+      </main>
+    </Fragment>
   );
 }
 
